16e93d73a1658fd2f7536b56583e8eb7f5303679
gideon_alpha <gideonalpha.dev@gmail.com>
2023-04-11 14:57:03 -0500

    - Added mechanism to split messages that are larger than the Meshtastic   message frame (whatever that is) and send them in parts

--- mesh_connection.py
+++ mesh_connection.py
@@ -21,13 +21,53 @@ class MeshConnection:
             interface = meshtastic.serial_interface.SerialInterface(devPath=_devPath);
         lock.release();
 
-    def send_text_to_mesh(self,text):
-        lock.acquire();
+    def __try_send(self,text,more_text=None):
         interface.sendText(text);
-        lock.release();
+        if more_text is not None:
+            interface.sendText(more_text);
+        
+    def send_text_to_mesh(self,text,lockit=True):
+        #note we assume failure unless we complete the try: block
+        success=False;
+
+        #dont lock on recursion
+        if lockit:
+            lock.acquire();
+        
+        #try to send text as-is
+        try:
+            self.__try_send(text);
+            success=True;
+        
+        #on exception, it's possible the text is too long
+        except Exception as ex:
+            #split text and try to split into two parts
+            a,b=self.__split_text(text);
+            if self.send_text_to_mesh(a,False):
+                if self.send_text_to_mesh(b,False):
+                    success=True
+        if lockit:
+            lock.release();
+        
+        return success;
 
     def close():
         lock.acquire();
         interface.close();
         lock.release();
+
+    def __split_text(self,long_text):
+        stringLen=len(long_text);
+        if (stringLen < 2):
+            raise Exception("Input string too short to split further");
+    
+        if stringLen%2 == 0:
+            firstString = long_text[0:stringLen//2]
+            secondString = long_text[stringLen//2:]
+
+        else:
+            firstString = long_text[0:(stringLen//2+1)]
+            secondString = long_text[(stringLen//2+1):]
+        
+        return(firstString,secondString);
 
